8dd1e2edc5e874620d17abe4e4d8aeb47189f38a
Keven <keven.mck@gmail.com>
2023-02-21 02:10:34 +0000

    re ad test

--- curriculum-front/tests/unit/setupTests.js
+++ curriculum-front/tests/unit/setupTests.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuetify from 'vuetify'
 import { createLocalVue } from '@vue/test-utils'
 
-console.log("****************************************************************************************")
 Vue.use(Vuetify)
 Vue.config.productionTip = false
 
